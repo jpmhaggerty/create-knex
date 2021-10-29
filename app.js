e4f16910c1f43367d429e0f305ed7d162424d933
James Haggerty <jpm.haggerty@gmail.com>
2021-10-29 12:45:30 -0400

    CFU 1 Complete

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const knex = require('knex')(require('path/to/knexfile.js')[process.env.NODE_ENV||'development']);
+const knex = require('knex')(require('./knexfile.js')[process.env.NODE_ENV||'development']);
 
 app.use(express.json());
 
